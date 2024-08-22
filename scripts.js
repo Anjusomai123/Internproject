bbdf83a19242289b0b99963f0277213036c6be9c
Anjusomai123 <anjusomai135@gnmail.com>
2024-08-22 15:54:07 +0545

    Project1

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var modal = document.getElementById("signin-modal")
+var btn = document.getElementById("signIn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebarMenu");
+    if (sidebar.style.display === "none" || sidebar.style.display === "") {
+        sidebar.style.display = "block";
+    } else {
+        sidebar.style.display = "none";
+    }
+}
